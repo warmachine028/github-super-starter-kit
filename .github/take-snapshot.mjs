cc4c62073c77ff75e6a1c698003ded7b5d9f6ad7
warmachine028 <warmachine028@outlook.com>
2024-07-29 20:47:16 +0530

    feat: updated take-snapshot.mjs

--- .github/take-snapshot.mjs
+++ .github/take-snapshot.mjs
@@ -1,19 +1,15 @@
-import puppeteer from "puppeteer";
-const sleep = (ms) => new Promise((res) => setTimeout(res, ms));
+import puppeteer from 'puppeteer'
+const sleep = (ms) => new Promise((res) => setTimeout(res, ms))
 
 const takeSnapShot = async () => {
-	const url = "https://github.com/warmachine028/github-super-starter-kit"; // Replace with your actual deployed URL
-	const browser = await puppeteer.launch();
-	const page = await browser.newPage();
-	await page.setViewport({ width: 1920, height: 1080 });
-	await page.goto(url);
-	await sleep(3000);
-	await page.screenshot({
-		path: ".github/preview.png",
-		fullPage: true,
-		captureBeyondViewport: true,
-	});
-	await browser.close();
-};
+    const url = 'https://github.com/warmachine028/github-super-starter-kit' // Replace with your actual deployed URL
+    const browser = await puppeteer.launch()
+    const page = await browser.newPage()
+    await page.setViewport({ width: 1920, height: 1080 })
+    await page.goto(url)
+    await sleep(3000)
+    await page.screenshot({ path: '.github/preview.png' })
+    await browser.close()
+}
 
-takeSnapShot();
+takeSnapShot()
